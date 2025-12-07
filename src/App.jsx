61e2e115ba61e9a42fe386eaa6eda717c3815740
Tasnim Tanha <tasnimakthertanha@gmail.com>
2025-12-07 07:49:12 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-5xl font-bold text-red-500">
+        Tailwind is working 🎉
+      </h1>
+    </div>
+  );
+}
+
+export default App;
